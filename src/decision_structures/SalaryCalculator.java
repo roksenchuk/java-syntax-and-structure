32ed4fe15b1fdd4c8184a13f47fb2f3c612c66bf
Rostyslav Oksenchuk <rostyslav.oksenchuk@flexspring.com>
2024-11-07 14:24:19 -0500

    Pasted init code to the class

--- src/decision_structures/SalaryCalculator.java
+++ src/decision_structures/SalaryCalculator.java
@@ -1,4 +1,26 @@
 package decision_structures;
 
+import java.util.Scanner;
+
 public class SalaryCalculator {
+
+    public static void main(String[] args) {
+        // Initialize known values
+        int salary = 1000;
+        int bonus = 250;
+        int quota = 10;
+
+        // Get values for the unknown
+        System.out.println("How many sales did employee did make this week?");
+        Scanner scanner = new Scanner(System.in);
+        int sales = scanner.nextInt();
+        scanner.close();
+
+        // Quick detour for the bonus earners
+
+
+
+        // Output
+        System.out.println("The employee's pay is $" + salary);
+    }
 }
