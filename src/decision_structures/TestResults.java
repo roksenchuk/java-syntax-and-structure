cccf68098fd1150d17339615c5e1275ba561899b
Rostyslav Oksenchuk <rostyslav.oksenchuk@flexspring.com>
2024-11-07 15:48:26 -0500

    Implemented TestResults.java

--- src/decision_structures/TestResults.java
+++ src/decision_structures/TestResults.java
@@ -16,6 +16,23 @@ public class TestResults {
         scanner.close();
 
         // Determine the letter grade
+        char grade;
+        if (score >= 90) {
+            grade = 'A';
+        }
+        else if (score >= 80) {
+            grade = 'B';
+        }
+        else if (score >= 70) {
+            grade = 'C';
+        }
+        else if (score >= 60) {
+            grade = 'D';
+        }
+        else {
+            grade = 'F';
+        }
 
+        System.out.println("Your grade is: " + grade);
     }
 }
