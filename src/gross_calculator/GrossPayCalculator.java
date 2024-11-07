4ce5498b504200fca4a6ccd9580cdfea0a6c680d
Rostyslav Oksenchuk <rostyslav.oksenchuk@flexspring.com>
2024-11-07 13:41:50 -0500

    Implemented user input for hours var

--- src/gross_calculator/GrossPayCalculator.java
+++ src/gross_calculator/GrossPayCalculator.java
@@ -1,9 +1,15 @@
 package gross_calculator; // package declaration
 
+import java.util.Scanner;
+
 public class GrossPayCalculator {
     public static void main(String[] args) {
         //1. Get the number of hours worked
-        int hours = 40;
+        int hours;
+        System.out.println("How many hours did you work?");
+
+        Scanner scanner = new Scanner(System.in);
+        hours = scanner.nextInt();
 
         //2. Get the hourly pay rate
         double payRate = 25.50;
