28888b7d051612a566afee08ffe30927def81aa9
Rostyslav Oksenchuk <rostyslav.oksenchuk@flexspring.com>
2024-11-07 18:20:44 -0500

    Implemented loop in AddNumbers

--- src/loops/AddNumbers.java
+++ src/loops/AddNumbers.java
@@ -1,8 +1,25 @@
 package loops;
 
+import java.util.Scanner;
+
 public class AddNumbers {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int runAgain = 0;
+        do {
+            System.out.println("Enter a first number: ");
+            double number1 = scanner.nextDouble();
+
+            System.out.println("Enter a second number: ");
+            double number2 = scanner.nextDouble();
+
+            double sum = number1 + number2;
+            System.out.println("The sum of " + number1 + " and " + number2 + " is " + sum);
+
+            System.out.println("Do you want to continue? Press 1 for Yes, 0 for No");
+            runAgain = scanner.nextInt();
+        } while (runAgain == 1);
     }
 }
