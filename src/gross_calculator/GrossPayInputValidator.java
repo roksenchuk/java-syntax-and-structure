eeafb7d7285cdb594383532c16a770fa4b32dbfa
Rostyslav Oksenchuk <rostyslav.oksenchuk@flexspring.com>
2024-11-07 18:05:11 -0500

    Created GrossPayInputValidator + initial implementation

---
+++ src/gross_calculator/GrossPayInputValidator.java
@@ -0,0 +1,28 @@
+package gross_calculator;
+
+import java.util.Scanner;
+
+/*
+ * WHILE LOOP
+ * Each store employee makes $15 USD an hour. Write a program that allows the employee
+ * to enter the number of hours worked for the week. Do not allow overtime
+ */
+public class GrossPayInputValidator {
+
+    public static void main(String[] args) {
+        // Initialize known variables
+        int payRate = 15;
+        int maxHours = 40;
+
+        // Get input for unknown variables
+        System.out.println("How many hours did employee work this week?");
+        Scanner scanner = new Scanner(System.in);
+        double hoursWorked = scanner.nextDouble();
+
+        scanner.close();
+
+        // Calculate gross
+        double gross = payRate * hoursWorked;
+        System.out.println("Gross pay: $" + gross);
+    }
+}
