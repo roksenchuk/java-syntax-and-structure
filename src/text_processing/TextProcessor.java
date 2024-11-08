8bbf7a0cf686bdb51039613bb32895b2c4433302
Rostyslav Oksenchuk <rostyslav.oksenchuk@flexspring.com>
2024-11-08 16:26:11 -0500

    Created + initial implement of TextProcessor

---
+++ src/text_processing/TextProcessor.java
@@ -0,0 +1,23 @@
+package text_processing;
+
+public class TextProcessor {
+
+    public static void main(String[] args) {
+        countWords("I love Java");
+    }
+
+    /**
+     * Splits a String into an array by tokenizing it
+     * Counts words and prints them
+     * @param text Full string to be split
+     */
+    public static void countWords(String text) {}
+
+    /**
+     * Prints String in the reverse order
+     * @param text Full string to be reversed
+     */
+    public static void reverseString(String text) {
+
+    }
+}
