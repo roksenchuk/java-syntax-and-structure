8c6c337214c8760066830d1e47b7948071c2cc1a
Rostyslav Oksenchuk <rostyslav.oksenchuk@flexspring.com>
2024-11-08 13:37:32 -0500

    Created + initial implement of HomeAreaCalculatorWithMethods

---
+++ src/objects/HomeAreaCalculatorWithMethods.java
@@ -0,0 +1,10 @@
+package objects;
+
+public class HomeAreaCalculatorWithMethods {
+
+    public static void main(String[] args) {
+
+        Rectangle kitchen = new Rectangle(200, 400);
+        Rectangle bathroom = new Rectangle(300, 700);
+    }
+}
