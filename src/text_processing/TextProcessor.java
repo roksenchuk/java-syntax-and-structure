7695c0f79b90c374d7997b10fe89c884fb335e48
Rostyslav Oksenchuk <rostyslav.oksenchuk@flexspring.com>
2024-11-08 16:32:13 -0500

    Implemented wordCount method

--- src/text_processing/TextProcessor.java
+++ src/text_processing/TextProcessor.java
@@ -11,7 +11,18 @@ public class TextProcessor {
      * Counts words and prints them
      * @param text Full string to be split
      */
-    public static void countWords(String text) {}
+    public static void countWords(String text) {
+
+        String[] words = text.split(" ");
+        int numberOfWords = words.length;
+        String message = String.format("Your String contains %d words", numberOfWords);
+        System.out.println(message);
+
+        for (int i = 0; i < numberOfWords; i++) {
+            String word = words[i];
+            System.out.println(word);
+        }
+    }
 
     /**
      * Prints String in the reverse order
