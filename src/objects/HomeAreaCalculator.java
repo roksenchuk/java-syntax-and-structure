c67abb7ae219aadf453b05a93d1c8389bc15df47
Rostyslav Oksenchuk <rostyslav.oksenchuk@flexspring.com>
2024-11-08 13:09:39 -0500

    Created + initial implement of HomeAreaCalculator

---
+++ src/objects/HomeAreaCalculator.java
@@ -0,0 +1,12 @@
+package objects;
+
+/*
+ * Write a class that creates instances of the 'Rectangle'
+ * class to fond the total area of two rooms in a house.
+ */
+public class HomeAreaCalculator {
+
+    public static void main(String[] args) {
+
+    }
+}
