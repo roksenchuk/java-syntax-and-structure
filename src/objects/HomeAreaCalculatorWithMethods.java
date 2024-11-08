693c1ade49cc002f1d38fb1ca8b9c6a29e726578
Rostyslav Oksenchuk <rostyslav.oksenchuk@flexspring.com>
2024-11-08 14:07:53 -0500

    Reimplemented calculations with method getRoom()

--- src/objects/HomeAreaCalculatorWithMethods.java
+++ src/objects/HomeAreaCalculatorWithMethods.java
@@ -1,10 +1,34 @@
 package objects;
 
+import java.util.Scanner;
+
 public class HomeAreaCalculatorWithMethods {
+
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        Rectangle kitchen = new Rectangle(200, 400);
-        Rectangle bathroom = new Rectangle(300, 700);
+        Rectangle kitchen = getRoom();
+        Rectangle bathroom = getRoom();
+
+        double area = calculateTotalArea(kitchen, bathroom);
+        System.out.println("the total area is: " + area);
+
+        scanner.close();
+    }
+
+    public static double calculateTotalArea(Rectangle rectangle1, Rectangle rectangle2) {
+
+        return rectangle1.calculateArea() + rectangle2.calculateArea();
+    }
+
+    public static Rectangle getRoom() {
+        System.out.println("Enter the length of your room:");
+        double length = scanner.nextDouble();
+
+        System.out.println("Enter the width of your room:");
+        double width = scanner.nextDouble();
+
+        return new Rectangle(length, width);
     }
 }
