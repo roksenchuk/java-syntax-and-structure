05852982035f3f1e616de2f4931d3fdb71c263f3
Rostyslav Oksenchuk <rostyslav.oksenchuk@flexspring.com>
2024-11-07 19:43:52 -0500

    Created AverageTestScores + initial implementation

---
+++ src/loops/AverageTestScores.java
@@ -0,0 +1,21 @@
+package loops;
+
+import java.util.Scanner;
+
+/*
+ * NESTED LOOPS:
+ * Find the average of each student's test scores
+ */
+public class AverageTestScores {
+
+    public static void main(String[] args) {
+        // Initialize what we know
+        int numberOfStudents = 24;
+        int numberOfTests = 4;
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        scanner.close();
+    }
+}
