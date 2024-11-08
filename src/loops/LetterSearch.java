a76df4fc971bbf463b05c2f960d6d437173a9fd9
Rostyslav Oksenchuk <rostyslav.oksenchuk@flexspring.com>
2024-11-07 20:07:04 -0500

    Added LetterSearch + initial implementation

---
+++ src/loops/LetterSearch.java
@@ -0,0 +1,20 @@
+package loops;
+
+import java.util.Scanner;
+
+/*
+ * BREAK STATEMENT
+ * Search a String to determine if it contains the letter 'A'
+ */
+public class LetterSearch {
+
+    public static void main(String[] args) {
+        // Get text
+        System.out.println("Enter some text: ");
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.next();
+        scanner.close();
+
+        boolean letterFound = false;
+    }
+}
