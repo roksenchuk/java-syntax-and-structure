72c882c65fb12b9b22e090936be6d41ac809d09d
Rostyslav Oksenchuk <rostyslav.oksenchuk@flexspring.com>
2024-11-07 22:55:38 -0500

    Created greetings package + Greetings class + initial implement

---
+++ src/methods/Greetings.java
@@ -0,0 +1,21 @@
+package methods;
+
+import java.util.Scanner;
+
+/*
+ * OUR FIRST METHOD
+ * Write a method that greets a user by name
+ */
+public class Greetings {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void greetUser() {
+        System.out.println("Enter your name:");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name + "!");
+    }
+}
