530d50453aeddcce47f67295671ad833b128761e
Rostyslav Oksenchuk <rostyslav.oksenchuk@flexspring.com>
2024-11-08 13:07:29 -0500

    Implemented Rectangle class

---
+++ src/objects/Rectangle.java
@@ -0,0 +1,35 @@
+package objects;
+
+public class Rectangle {
+    // length field
+    private double length;
+    public double getLength() {
+        return length;
+    }
+    public void setLength(double length) {
+        this.length = length;
+    }
+    // width field
+    private double width;
+    public double getWidth() {
+        return width;
+    }
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    // constructor
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    // calculate perimeter method
+    double calculatePerimeter() {
+        return 2 * length + 2 * width;
+    }
+    // calculate area method
+    double calculateArea() {
+        return length * width;
+    }
+}
